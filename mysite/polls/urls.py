10b2d54ad9aea8b262828f4b9daa18677d1b4621
sl846 <sl846@vcm-38454.vm.duke.edu>
2024-01-31 22:49:28 -0500

    Django tutorial part 6

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,3 +1,4 @@
+""" hard-way to write views
 from unittest import result
 from django.urls import path
 from . import views
@@ -11,5 +12,32 @@ urlpatterns = [
     # ex: /polls/5/results/
     path("<int:question_id>/results/", views.results, name="results"),
     # ex: /polls/5/vote/
+    path("<int:question_id>/vote/", views.vote, name="vote"),
+]
+"""
+
+"""
+A common case of basic web development: 
+    - getting data from the database according to a parameter passed in the URL
+    - loading a template, and 
+    - returning the rendered template. 
+Because this is so common, Django provides a shortcut, 
+called the "generic views" syetem.
+
+Generic views abstract common patterns to the point where you don't even need to 
+write Python code to write an app. For example, the ListView and DetailView generic
+views abstract the concepts of "display a list of objects" and "display a detail
+page for a particular type of object" respectively.
+"""
+
+from django.urls import path
+from django.views import View
+from . import views
+
+app_name = "polls"
+urlpatterns = [
+    path("", views.IndexView.as_view(), name="index"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
     path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,3 +1,4 @@
+"""
 from re import template
 from urllib import response
 from django.shortcuts import render, get_object_or_404
@@ -37,6 +38,35 @@ def results(request, question_id):
     # return HttpResponse(response % question_id)
     question = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/results.html", {"question": question})
+"""
+
+from typing import Any
+from django.db.models.query import QuerySet
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+
+from .models import Question, Choice
+
+
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self) -> QuerySet[Any]:
+        # return the last five published questions
+        return Question.objects.order_by("-pub_date")[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 
 def vote(request, question_id):
