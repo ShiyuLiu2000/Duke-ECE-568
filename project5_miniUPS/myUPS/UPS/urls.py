4c933a9a6033918ee4ea2ce8302e0790e346314b
sl846 <shiyu.liu2@duke.edu>
2024-04-21 17:43:16 -0400

    add login and signup

--- project5_miniUPS/myUPS/UPS/urls.py
+++ project5_miniUPS/myUPS/UPS/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("frontpage/", views.frontpage, name="frontpage"),
+    path("login/", views.login_view, name="login"),
+    path("signup/", views.signup_view, name="signup"),
 ]
--- project5_miniUPS/myUPS/UPS/views.py
+++ project5_miniUPS/myUPS/UPS/views.py
@@ -1,4 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from .forms import SignUpForm
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
+
 
 def frontpage(request):
-    return render(request, 'UPS/frontpage.html')
+    return render(request, "UPS/frontpage.html")
+
+
+def signup_view(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+    else:
+        form = SignUpForm()
+    return render(request, "UPS/signup.html", {"form": form})
+
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect("myPackages")
+            else:
+                form.add_error(None, "Invalid username or password.")
+    else:
+        form = AuthenticationForm()
+
+    return render(request, "UPS/login.html", {"form": form})
